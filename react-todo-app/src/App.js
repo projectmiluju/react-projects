19e3197fcb7f86ace2e9dfbeabfaeb5565f4d986
projectmiluju <dnjsdyd0712@naver.com>
2024-09-23 11:51:06 +0900

    feat: 할일목록 추가 ui 생성

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -58,6 +58,21 @@ export default class App extends Component {
               </p>
             </div>
           ))}
+          <form style={{ display: "flex" }}>
+            <input
+              type="text"
+              name="value"
+              style={{ flex: "10", padding: "5px" }}
+              placeholder="해야 할 일을 입력하세요."
+              value=""
+            />
+            <input
+              type="submit"
+              value="입력"
+              className="btn"
+              style={{ flex: "1" }}
+            />
+          </form>
         </div>
       </div>
     );
