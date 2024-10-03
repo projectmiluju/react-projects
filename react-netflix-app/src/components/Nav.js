46b9ddcfa0bb531c885b3bbc568ef2bc9fe2fe0a
projectmiluju <dnjsdyd0712@naver.com>
2024-10-03 12:29:08 +0900

    feat(netflix) : NavBar 스크롤시 색 변경

--- react-netflix-app/src/components/Nav.js
+++ react-netflix-app/src/components/Nav.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Nav.css";
 
 export default function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 50) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
   return (
-    <nav>
+    <nav className={`nav ${show && "nav__black"}`}>
       <img
         alt="Netflix logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
