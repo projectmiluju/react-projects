5d923880e849e30f95aba127d10554a9273bca41
projectmiluju <dnjsdyd0712@naver.com>
2024-10-04 14:13:17 +0900

    feat(netflix) : 배너 ui 생성

--- react-netflix-app/src/components/Banner.js
+++ react-netflix-app/src/components/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "../api/axios";
 import requests from "../api/requests";
+import "./Banner.css";
 
 export default function Banner() {
   const [movie, setMovie] = useState([]);
@@ -22,5 +23,36 @@ export default function Banner() {
     });
     setMovie(movieDetail);
   };
-  return <div></div>;
+
+  const truncate = (string, number) => {
+    return string?.length > number
+      ? string.substr(0, number - 1) + "..."
+      : string;
+  };
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition: "top center",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="banner__contents">
+        {/**Title */}
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner_buttons">
+          <button className="banner__button play">Play</button>
+          <button className="banner__button info">
+            <div className="space"></div> More Information
+          </button>
+        </div>
+        {/**DIV > 2 BUTTONS */}
+        <h1 className="banner__description">{truncate(movie?.overview, 10)}</h1>
+      </div>
+      <div className="banner__fadeBottom" />
+    </header>
+  );
 }
