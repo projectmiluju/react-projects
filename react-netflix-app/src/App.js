fff8f0a738a1acf5ea2b5cb794a8e9a0c230ebff
projectmiluju <dnjsdyd0712@naver.com>
2024-10-06 21:25:59 +0900

    feat(netflix) : footer 생성

--- react-netflix-app/src/App.js
+++ react-netflix-app/src/App.js
@@ -3,6 +3,7 @@ import Nav from "./components/Nav";
 import Banner from "./components/Banner";
 import Row from "./components/Row";
 import requests from "./api/requests";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
         id="CM"
         fetchUrl={requests.fetchComedyMovies}
       ></Row>
+      <Footer />
     </div>
   );
 }
