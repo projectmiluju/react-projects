5d49b10e1d739621df4f3dd75d74d839149ff06d
projectmiluju <dnjsdyd0712@naver.com>
2024-10-07 03:22:55 +0900

    feat(netflix) : searchterm

--- react-netflix-app/src/pages/SearchPage/index.js
+++ react-netflix-app/src/pages/SearchPage/index.js
@@ -1,5 +1,31 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "../../api/axios";
+import "./SearchPage.css";
 export default function SearchPage() {
-  return <div>SearchPage</div>;
+  const [searchResults, setSearchResults] = useState([]);
+
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+  let query = useQuery();
+  const searchTerm = query.get("q");
+  useEffect(() => {
+    if (searchTerm) {
+      fetchSearchMovie(searchTerm);
+    }
+  }, [searchTerm]);
+
+  const fetchSearchMovie = async (searchTerm) => {
+    try {
+      const request = await axios.get(
+        `/search/multi?include_adult=false&query=${searchTerm}`
+      );
+      setSearchResults(request.data.results);
+    } catch (error) {
+      console.error("Error fetching search results", error);
+    }
+  };
+
+  return <div></div>;
 }
