0e26e6f376fa692287e74d1e31e6649a1b7dd57a
projectmiluju <dnjsdyd0712@naver.com>
2024-10-07 02:49:07 +0900

    feat(netflix) : navbar에 Input 생성

--- react-netflix-app/src/components/Nav.js
+++ react-netflix-app/src/components/Nav.js
@@ -1,8 +1,16 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Nav.css";
 
 export default function Nav() {
   const [show, handleShow] = useState(false);
+
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    navigate(`/search?q=${e.target.value}`);
+  };
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -23,6 +31,13 @@ export default function Nav() {
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
         className="nav__logo"
         onClick={() => window.location.reload()}
+      />
+      <input
+        value={searchValue}
+        onChange={handleChange}
+        className="nav__input"
+        type="text"
+        placeholder="영화를 검색해주세요."
       />
       <img
         alt="User logged"
