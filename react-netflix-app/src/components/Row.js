bd19914238d335b566eb7303139d38004fdc370f
projectmiluju <dnjsdyd0712@naver.com>
2024-10-09 16:16:39 +0900

    feat(netflix) : swiper 모듈 설치 및 적용

--- react-netflix-app/src/components/Row.js
+++ react-netflix-app/src/components/Row.js
@@ -2,6 +2,13 @@ import React, { useState, useEffect } from "react";
 import axios from "../api/axios";
 import "./Row.css";
 import MovieModal from "./MovieModal/index.js";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 export default function Row({ isLargeRow, title, fetchUrl, id }) {
   const [movies, setMovies] = useState([]);
@@ -26,42 +33,47 @@ export default function Row({ isLargeRow, title, fetchUrl, id }) {
   return (
     <section className="row">
       <h2>{title}</h2>
-      <div className="slider">
-        <div className="slider__arrow-left">
-          <span
-            className="arrow"
-            onClick={() => {
-              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
-            }}
-          >
-            {"<"}
-          </span>
-        </div>
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        navigation
+        pagination={{ clickable: true }}
+        loop={true}
+        breakpoints={{
+          1378: {
+            slidesPerView: 6,
+            spaceBetween: 6,
+          },
+          998: {
+            slidesPerView: 5,
+            spaceBetween: 5,
+          },
+          625: {
+            slidesPerView: 4,
+            spaceBetween: 4,
+          },
+          0: {
+            slidesPerView: 3,
+            spaceBetween: 3,
+          },
+        }}
+      >
         <div id={id} className="row__posters">
           {movies.map((movie) => (
-            <img
-              key={movie.id}
-              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-              src={`https://image.tmdb.org/t/p/original/${
-                isLargeRow ? movie.poster_path : movie.backdrop_path
-              }`}
-              loading="lazy"
-              alt={movie.name}
-              onClick={() => handleClick(movie)}
-            />
+            <SwiperSlide>
+              <img
+                key={movie.id}
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                src={`https://image.tmdb.org/t/p/original/${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                loading="lazy"
+                alt={movie.name}
+                onClick={() => handleClick(movie)}
+              />
+            </SwiperSlide>
           ))}
         </div>
-        <div className="slider__arrow-right">
-          <span
-            className="arrow"
-            onClick={() => {
-              document.getElementById(id).scrollLeft += window.innerWidth - 80;
-            }}
-          >
-            {">"}
-          </span>
-        </div>
-      </div>
+      </Swiper>
 
       {modalOpen && (
         <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
