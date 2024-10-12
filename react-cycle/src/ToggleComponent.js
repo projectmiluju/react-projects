66a91bc3823dae69aeb72a802eceef36ee4929b3
projectmiluju <dnjsdyd0712@naver.com>
2024-10-12 17:21:54 +0900

    feat(exp) : 컴포넌트 라이프 사이클 useEffect

--- react-cycle/src/ToggleComponent.js
+++ react-cycle/src/ToggleComponent.js
@@ -2,14 +2,14 @@ import React, { useEffect } from "react";
 
 const ToggleComponent = () => {
   useEffect(() => {
-    console.log("🟢 컴포넌트가 마운트되었습니다!");
+    console.log("컴포넌트가 마운트");
 
     return () => {
-      console.log("🔴 컴포넌트가 언마운트되었습니다!");
+      console.log("컴포넌트가 언마운트");
     };
   }, []);
 
-  return <h2>✨ 나는 토글 컴포넌트야!</h2>;
+  return <h2>컴포넌트</h2>;
 };
 
 export default ToggleComponent;
