15222adb94811db622fdd2685388f956e7c949bd
projectmiluju <dnjsdyd0712@naver.com>
2024-10-17 06:10:50 +0900

    feat(button) : +,- test

--- react-button-app/src/App.test.js
+++ react-button-app/src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("the counter starts at 0", () => {
@@ -23,4 +23,26 @@ test("plus button has correct text", () => {
   const buttonElement = screen.getByTestId("plus-button");
 
   expect(buttonElement).toHaveTextContent("+");
+});
+
+test("When the + button is pressed, the counter changes to 1", () => {
+  render(<App />);
+
+  const buttonElement = screen.getByTestId("plus-button");
+
+  fireEvent.click(buttonElement);
+
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(1);
+});
+
+test("When the - button is pressed, the counter changes to -1", () => {
+  render(<App />);
+
+  const buttonElement = screen.getByTestId("minus-button");
+
+  fireEvent.click(buttonElement);
+
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(-1);
 });
