cfe29daba4d75b1aebf19a6450526d4070fb6932
projectmiluju <dnjsdyd0712@naver.com>
2024-10-17 05:57:13 +0900

    feat(button) : button test 코드 작성

--- react-button-app/src/App.test.js
+++ react-button-app/src/App.test.js
@@ -8,3 +8,19 @@ test("the counter starts at 0", () => {
 
   expect(counterElement).toHaveTextContent(0);
 });
+
+test("minus button has correct text", () => {
+  render(<App />);
+
+  const buttonElement = screen.getByTestId("minus-button");
+
+  expect(buttonElement).toHaveTextContent("-");
+});
+
+test("plus button has correct text", () => {
+  render(<App />);
+
+  const buttonElement = screen.getByTestId("plus-button");
+
+  expect(buttonElement).toHaveTextContent("+");
+});
