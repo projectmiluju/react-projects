a79ad281edd9fccd240395b140112f8a54b4a7b6
projectmiluju <dnjsdyd0712@naver.com>
2024-10-27 17:46:00 +0900

    feat: css를 이용하여 스타일링

---
+++ react-styling/src/App.js
@@ -0,0 +1,13 @@
+import ButtonCss from "./components/ButtonCss.jsx";
+
+
+function App() {
+  return (
+      <div style={{ padding: 50 }}>
+        <h1>React 스타일링</h1>
+        <ButtonCss />
+      </div>
+  );
+}
+
+export default App;
---
+++ react-styling/src/components/ButtonCss.jsx
@@ -0,0 +1,5 @@
+import "./Button.css";
+
+export default function ButtonCSS() {
+    return <button className="btn-css">CSS 버튼</button>;
+}
