38e3d88ba6f785086dcc5fb42ddf345b3fa28604
projectmiluju <dnjsdyd0712@naver.com>
2024-10-31 02:10:30 +0900

    feat(shop) : msw 이용 테스트 코드

---
+++ react-shop-test/src/pages/OrderPage/Products.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Products({ name, imagePath }) {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <img
+        style={{ width: "75%" }}
+        src={`http://localhost:5000/${imagePath}`}
+        alt={`${name} product`}
+      />
+      <form style={{ marginTop: "10px" }}>
+        <label htmlFor={name} style={{ textAlign: "right" }}>
+          {name}
+        </label>
+        <input
+          id={name}
+          style={{ marginLeft: 7 }}
+          type="number"
+          name="quantity"
+          min="0"
+          defaultValue={0}
+        />
+      </form>
+    </div>
+  );
+}
+
+export default Products;
