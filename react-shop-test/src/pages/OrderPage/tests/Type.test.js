b615e23f5c0c4fbc046b6950e6c5265f863c58c2
projectmiluju <dnjsdyd0712@naver.com>
2024-11-01 09:59:18 +0900

    feat(shop) : 에러 발생 처리

--- react-shop-test/src/pages/OrderPage/tests/Type.test.js
+++ react-shop-test/src/pages/OrderPage/tests/Type.test.js
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
+import { server } from "../../../mocks/server";
 import Type from "../Type";
+import { rest } from "msw";
 
 test("displays product images from server", async () => {
   render(<Type orderType="products" />);
@@ -12,4 +14,17 @@ test("displays product images from server", async () => {
 
   const altText = productImages.map((element) => element.alt);
   expect(altText).toEqual(["America product", "England product"]);
+});
+
+test("when fetching product datas, face an error", async () => {
+  server.resetHandlers(
+    rest.get("http://localhost:5000/products", (req, res, ctx) => {
+      return res(ctx.status(500));
+    })
+  );
+
+  render(<Type orderType="products" />);
+
+  const errorBanner = await screen.findByTestId("error-banner");
+  expect(errorBanner).toHaveTextContent("에러가 발생했습니다.");
 });
